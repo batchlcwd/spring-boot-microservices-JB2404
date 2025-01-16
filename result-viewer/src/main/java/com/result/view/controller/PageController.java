33b86bef815ad7371018db32f885b71847efeb07
batchlcwd <batchlcwd@gmail.com>
2025-01-17 00:03:10 +0530

    16:01:2024 result viewer | mini project

---
+++ result-viewer/src/main/java/com/result/view/controller/PageController.java
@@ -0,0 +1,27 @@
+package com.result.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+    @GetMapping("/view-result")
+    public String viewResultForm(){
+        return "view_result_form";
+    }
+
+    @GetMapping("/help")
+    public String help(){
+        return "help";
+    }
+
+
+}
