11b16bef7ab7dd133e5d94ab983fed5ee442421f
batchlcwd <batchlcwd@gmail.com>
2025-02-13 23:06:05 +0530

    13:02:2025 | creating microservices of substring foodie app

---
+++ microservices/payement-delivery-service/src/main/java/com/substring/foodie/payment/PayementDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.substring.foodie.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayementDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayementDeliveryServiceApplication.class, args);
+	}
+
+}
